d7f8d07ecddad081e9e2e9d33c2554a64c4320de
Thomas J Bradley <hey@thomasjbradley.ca>
2015-04-14 11:24:17 -0400

    Add CSS template.

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,12 @@ gulp.task('css', function () {
   return gulp.src('css/main.css')
     .pipe(cssnext({ compress: true }))
     .pipe(autoprefixer({ cascade: false }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('tmp'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('css'))
   ;
 });
 
 gulp.task('watch', function() {
-  gulp.watch('css/**/*.css', ['css']);
+  gulp.watch('css/*.css', ['css']);
 });
