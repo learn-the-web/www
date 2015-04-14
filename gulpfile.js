caf5edfbfde7280138671d8bba44b7bca4146373
Thomas J Bradley <hey@thomasjbradley.ca>
2015-04-14 11:08:41 -0400

    Add Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var
+  gulp = require('gulp'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cssnext = require('gulp-cssnext'),
+  rename = require('gulp-rename')
+;
+
+gulp.task('css', function () {
+  return gulp.src('css/main.css')
+    .pipe(cssnext({ compress: true }))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('css'))
+  ;
+});
+
+gulp.task('watch', function() {
+  gulp.watch('css/**/*.css', ['css']);
+});
