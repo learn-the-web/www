728b9fabbca0b3a25a61a3d5e4f2b60dd70616fb
Thomas J Bradley <hey@thomasjbradley.ca>
2015-04-20 09:22:36 -0400

    Add keyboard arrow key support to page nav.

--- js/common.js
+++ js/common.js
@@ -58,10 +58,24 @@
     return;
   }
 
-  navCheck.addEventListener('keypress', function (e) {
+  navCheck.addEventListener('keydown', function (e) {
     switch (e.keyCode) {
-      case 13:
+      case 13: // return
         toggleNavCheck();
+        break;
+      case 27: // esc
+        navCheck.checked = false;
+        navCheck.focus();
+        break;
+      case 40: // down
+        e.preventDefault();
+        navCheck.checked = true;
+        document.querySelector('.nav-check ~ ol > li:first-child a').focus();
+        break;
+      case 38: // up
+        e.preventDefault();
+        navCheck.checked = true;
+        document.querySelector('.nav-check ~ ol > li:last-child a').focus();
         break;
     }
   });
@@ -70,5 +84,36 @@
 
   navList.addEventListener('click', function () {
     navCheck.checked = false;
+  });
+
+  navList.addEventListener('keydown', function (e) {
+    var focusedItem;
+
+    switch (e.keyCode) {
+      case 27: // esc
+        navCheck.checked = false;
+        navCheck.focus();
+        break;
+      case 40: // down
+        e.preventDefault();
+        focusedItem = document.querySelector('.nav-check ~ ol > li a:focus');
+
+        if (focusedItem.parentNode.nextElementSibling) {
+          focusedItem.parentNode.nextElementSibling.children[0].focus();
+        } else {
+          document.querySelector('.nav-check ~ ol > li:first-child a').focus();
+        }
+        break;
+      case 38: // up
+        e.preventDefault();
+        focusedItem = document.querySelector('.nav-check ~ ol > li a:focus');
+
+        if (focusedItem.parentNode.previousElementSibling) {
+          focusedItem.parentNode.previousElementSibling.children[0].focus();
+        } else {
+          document.querySelector('.nav-check ~ ol > li:last-child a').focus();
+        }
+        break;
+    }
   });
 }());
