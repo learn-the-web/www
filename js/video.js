11b63372cdbd0c216ff24b360219abcf8f8162bd
Thomas J Bradley <hey@thomasjbradley.ca>
2015-10-17 12:18:52 -0400

    Add ability to have single videos at top of article pages

--- js/video.js
+++ js/video.js
@@ -2,7 +2,8 @@
   "use strict";
 
   var
-    iframe = '<iframe class="embed__item video-embed__item video-embed__item--loading" src="https://www.youtube.com/embed/videoseries?list={id}&autoplay=1&color=white&theme=light" frameborder="0" allowfullscreen></iframe>',
+    iframeVideo = '<iframe class="embed__item video-embed__item video-embed__item--loading" src="https://www.youtube.com/embed/{id}&autoplay=1&color=white&theme=light" frameborder="0" allowfullscreen></iframe>',
+    iframePlaylist = '<iframe class="embed__item video-embed__item video-embed__item--loading" src="https://www.youtube.com/embed/videoseries?list={id}&autoplay=1&color=white&theme=light" frameborder="0" allowfullscreen></iframe>',
     playBtn = document.querySelector('.play-btn')
   ;
 
@@ -12,17 +13,25 @@
 
   playBtn.addEventListener('click', function (e) {
     var
-      playlist = this.getAttribute('data-playlist'),
+      videoId, iframe,
       videoEmbed = document.querySelector('.video-embed'),
       embedItem
     ;
 
     e.preventDefault();
 
+    if (this.hasAttribute('data-video')) {
+      videoId = this.getAttribute('data-video');
+      iframe = iframeVideo.replace('{id}', videoId);
+    } else {
+      videoId = this.getAttribute('data-playlist');
+      iframe = iframePlaylist.replace('{id}', videoId);
+    }
+
     videoEmbed.classList.add('is-loading');
     videoEmbed.classList.add('embed--16by9');
     videoEmbed.classList.remove('embed--3by1');
-    videoEmbed.innerHTML = iframe.replace('{id}', playlist);
+    videoEmbed.innerHTML = iframe;
     embedItem = document.querySelector('.video-embed__item');
     document.querySelector('h1').classList.add('is-playing');
     this.parentNode.removeChild(this);
