1a6f4519a21d0122e2810e84d362557de990ab0e
Thomas J Bradley <hey@thomasjbradley.ca>
2016-08-27 16:24:50 -0400

    Add an assessment view to the course page

---
+++ js-src/tabs.js
@@ -0,0 +1,32 @@
+(function () {
+  var tablist = document.querySelector('[role="tablist"]');
+  var tabs = document.querySelectorAll('[role="tab"]');
+
+  if (!tablist && !tabs) return;
+
+  tablist.removeAttribute('hidden');
+  tablist.removeAttribute('aria-hidden');
+
+  [].forEach.call(tabs, function (elem) {
+    elem.addEventListener('click', function (e) {
+      var id = e.target.getAttribute('aria-controls');
+      var oldTab = document.querySelector('[role="tab"][aria-selected="true"]');
+      var oldTabPanel = document.querySelector('[role="tabpanel"]:not([hidden])');
+      var newTabPanel = document.getElementById(id);
+
+      e.preventDefault();
+
+      oldTab.classList.remove('bold');
+      oldTab.removeAttribute('aria-selected');
+      oldTab.nextElementSibling.setAttribute('hidden', true);
+      oldTabPanel.setAttribute('hidden', true);
+      oldTabPanel.setAttribute('aria-hidden', true);
+
+      e.target.classList.add('bold');
+      e.target.setAttribute('aria-selected', true);
+      e.target.nextElementSibling.removeAttribute('hidden');
+      newTabPanel.removeAttribute('hidden');
+      newTabPanel.removeAttribute('aria-hidden');
+    });
+  });
+}());
