1f1269774f531e84efff397ceeaf5f0a4985bc6f
Thomas J Bradley <hey@thomasjbradley.ca>
2017-01-22 11:05:33 -0500

    Fix a link bug in the Markbot banners

--- js-src/markbot.js
+++ js-src/markbot.js
@@ -1,17 +1,20 @@
 (function () {
   'use strict';
 
+  var markbotBtnIcon = document.getElementById('markbot-btn-icon');
   var markbotBtnMac = document.getElementById('markbot-btn-mac');
   var markbotBtnWin = document.getElementById('markbot-btn-win');
   var isWin = (navigator.appVersion.indexOf('Win') != -1) ? true : false;
 
-  if (!markbotBtnMac && !markbotBtnWin) return;
+  if (!markbotBtnMac && !markbotBtnWin && !markbotIconBtn) return;
 
   if (isWin) {
     markbotBtnMac.setAttribute('hidden', true);
     markbotBtnWin.removeAttribute('hidden');
+    markbotBtnIcon.href = markbotBtnWin.querySelector('a').href;
   } else {
     markbotBtnMac.removeAttribute('hidden');
     markbotBtnWin.setAttribute('hidden', true);
+    markbotBtnIcon.href = markbotBtnMac.querySelector('a').href;
   }
 }());
